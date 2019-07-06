67fc0277bb27749d1f60dbca6ce98d701281f846
Juan Carlos Suazo Dirocie <jsuazo9@gmail.com>
2019-07-06 16:20:44 -0400

    added favorites test

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -12,21 +12,10 @@ class UsersTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
     /** @test */
-    public function a_auth_can_create_users()
+    public function a_auth_user_can_see_his_information()
     {
         $this->withoutExceptionHandling();
-        $attribues = [
-            'name' => $this->faker->name,
-            'last_name' => $this->faker->lastName,
-            'email' => $this->faker->unique()->safeEmail,
-            'role' => 'user',
-            'age' => $this->faker->numberBetween(10, 80),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
         Passport::actingAs(factory(\App\User::class)->create(['role' => 'admin']));
-        $this->post('/api/user', $attribues);
-        $this->assertDatabaseHas('users', $attribues);
+        $this->get('/api/user')->assertJsonStructure();
     }
 }
